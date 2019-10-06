63ca481834de05676377daa43c01186d13586d97
Martin Beier <Martin.Beier@gmx.de>
2019-10-06 21:35:47 +0200

    Erweiterung JavaScript

--- cid/form.js
+++ cid/form.js
@@ -4,15 +4,24 @@ var inputMail = document.getElementById('input-mail');
 var inputMessage = document.getElementById('input-message');
 var button = document.querySelector('.submit-button');
 var eMailRegEx = /^\S+@\S+\.\S+$/;
+var form = document.querySelector('.form');
 
 function checkInput() {
     if ( inputName.value.trim() !== "" &&
          eMailRegEx.test(inputMail.value) &&
          inputMessage.value.trim() !== "" ) {
+        button.disabled = false;
         button.style.backgroundColor = '#fa923f';
-        button.disable = false;
+        console.log('enabled');
     } else {
+        button.disabled = true;
         button.style.backgroundColor = '#979695';
-        button.disable = true;
+        console.log('disabled');
     }
 }
+
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); /* Standardverhalten "zum Server senden" unterdrücken */
+    console.log(event);
+})
